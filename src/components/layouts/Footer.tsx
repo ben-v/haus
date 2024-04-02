7528537367abcc3f6d83ea0561e60cbd41b6ebb2
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-02 13:02:04 -0600

    Added logo text and tag line. Adjusted alignment of items to be a top of element for the logo and social links.

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -22,12 +22,14 @@ const Footer: React.FC<{ sections: Section[] }> = ({ sections }) => {
         <div className="m-auto space-y-8 px-4 text-gray-600 dark:text-gray-400 sm:px-12 xl:max-w-6xl xl:px-0">
           <div className="grid grid-cols-8 gap-6 md:gap-0">
             <div className="col-span-8 md:col-span-2 lg:col-span-3">
-              <div className="flex h-full items-center justify-between gap-6 border-b border-white py-6 dark:border-gray-800 md:flex-col md:items-start md:justify-between md:space-y-6 md:border-none md:py-0">
+              <div className="flex h-full items-start justify-between gap-6 border-b border-white py-6 dark:border-gray-800 md:flex-col md:items-start md:justify-between md:space-y-6 md:border-none md:py-0">
                 <div>
                   <Link to="/" aria-label="HAUS Property Services Logo" className="flex items-center">
                     {/* <img className="h-7 w-auto " src="/logos/logo_medium_gray.svg" /> */}
                     {Icons.Logo(undefined, "h-8")}
                   </Link>
+                  <p className='pt-5'>HAUS Property Services</p>
+                  <p>A Bozeman, Montana Company.</p>
                 </div>
 
                 <div className="flex gap-6">
