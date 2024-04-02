868bc3fdce379c866b3879f203b611f452eeba3d
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-02 13:43:02 -0600

    Stubbed out page components for privacy, terms, and partners. Added links in footer.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { Route, Routes } from "react-router-dom"
 
 import Home from "./pages/home"
 import Contact from "./pages/contact";
+import Privacy from "./pages/legal/privacy";
+import Terms from "./pages/legal/terms";
 
 import ScrollToHashElementBehavior from "./components/behaviors/ScrollToHashElementBehavior"
 import Header from "./components/layouts/Header"
 import Footer from "./components/layouts/Footer"
 import GenericHttpError from "./components/errors/GenericHttpError";
+import Partners from "./pages/partners";
 
 interface Link {
   label: string;
@@ -44,11 +47,9 @@ function App() {
     {
       title: 'Resources',
       links: [
-        { label: 'About', url: '#' },
-        { label: 'Customers', url: '#' },
-        { label: 'Enterprise', url: '#' },
-        { label: 'Partners', url: '#' },
-        { label: 'Jobs', url: '#' }
+        { label: 'Privacy Policy', url: '/privacy' },
+        { label: 'Terms & Conditions', url: '/terms' },
+        { label: 'Partners', url: '/partners' },
       ]
     }
   ];
@@ -61,6 +62,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/partners" element={<Partners />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
         <Route path="/server-error" element={<GenericHttpError title="500" description="Well, this is embarassing, we encountered an unexpected error." />} />
         <Route path="*" element={<GenericHttpError title="404" description="Hmm, we did not find the page you requested."/>} />
       </Routes>
