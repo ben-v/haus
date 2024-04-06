98a41290b6832fa19f687ec9d4684afb599e0106
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-06 15:10:49 -0600

    Stubbed out landing page and sections files.

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,17 @@
+import Clients from './sections/Clients'
+import Services from './sections/Services'
+import ServiceArea from './sections/ServiceArea'
+import Faqs from './sections/Faqs'
+
+const Landing = () => {
+  return (
+    <>
+      <Clients />
+      <Services />
+      <ServiceArea />
+      <Faqs />
+    </>
+  )
+}
+
+export default Landing
---
+++ src/pages/landing/sections/Services.tsx
@@ -0,0 +1,23 @@
+const Services = () => {
+    return (
+      <section className="py-32 sm:pt-32 md:pt-32 lg:pt-32">
+        <div className="mx-auto px-4 sm:px-12 xl:max-w-5xl xl:px-0">
+          <div className="relative z-10 text-start md:mx-auto md:w-5/6 lg:w-4/6">
+            <h1 className="relative text-start text-4xl font-bold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">Services</h1>
+          </div>
+  
+          <div className="mt-12 grid gap-12 sm:mx-auto sm:max-w-lg lg:max-w-max lg:grid-cols-2">
+            <div className="relative">
+                <div className="absolute inset-0 hidden scale-105 rounded-3xl bg-gradient-to-b from-transparent dark:block dark:to-gray-900/80"></div>
+                <div className="relative">
+                  <h2 className="text-2xl font-bold text-gray-800 dark:text-white">To do</h2>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  
+    )
+  }
+  
+  export default Services
