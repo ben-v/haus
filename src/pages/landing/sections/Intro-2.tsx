8dd76de09261fb3591de5506f62774c6f6c523aa
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-11 16:10:39 -0600

    Updated landing image sizes and some copy.

--- src/pages/landing/sections/Intro-2.tsx
+++ src/pages/landing/sections/Intro-2.tsx
@@ -16,9 +16,9 @@ const Intro2 = () => {
                             <div className="p-2 w-auto text-center lg:text-left">
                                 <h2 className="text-3xl font-bold text-gray-900 dark:text-white lg:text-4xl">Where old school meets new</h2>
                             </div>
-                            <p className="mt-4 lg:mt-16 text-lg text-gray-600 dark:text-gray-300 sm:text-xl text-center lg:text-left">At <span className="font-semibold">HAUS</span> our approach with our clients is first and foremost is to help each and every one of them with their list of projects, big or small. With over over 30 years in high tech, at some of the largest companies in the United States, listening, communication & collaboration were the foundations of success then and are the foundation of success today on any project...in corporate America or in the home.</p>
+                            <p className="mt-4 lg:mt-16 text-lg text-gray-600 dark:text-gray-300 sm:text-xl text-center lg:text-left">At <span className="font-semibold">HAUS</span> our approach with our clients is first and foremost is to help each and every one of them with their list of projects, big or small. With over over 30 years in high tech, at some of the largest companies in the United States, respectful and considerate communication & collaboration were the foundations of success then and are the foundation of success today on any project...in corporate America or in the home.</p>
                         </div>
-                        <div className="lg:w-3/12 w-auto p-4 sm:pt-0 sm:pb-0 lg:-mt-20">
+                        <div className="lg:w-3/12 w-auto p-4">
                             {(theme === THEMES.DARK) ? <SvgIntroSuccessDark className="w-full" /> : <SvgIntroSuccessLight className="w-full" />}
                         </div>
                     </div>
