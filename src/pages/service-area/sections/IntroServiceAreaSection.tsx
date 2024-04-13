0a3a1d002a99a716c0ee00dc3bdf91b5745e77e4
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-13 07:28:14 -0600

    Updated copy

--- src/pages/service-area/sections/IntroServiceAreaSection.tsx
+++ src/pages/service-area/sections/IntroServiceAreaSection.tsx
@@ -6,7 +6,7 @@ const IntroServiceAreaSection = () => {
                     <div className="py-2 px-0 md:p-2 w-auto text-center">
                         {/* <h2 className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white lg:text-4xl">Helping our clients is <span className="bg-gradient-to-r from-primaryLight to-secondaryLight bg-clip-text text-transparent">our #1 job</span></h2> */}
                         <h1 className="relative text-center text-4xl font-bold text-gray-900 dark:text-white sm:text-5xl md:text-6xl"><span className="opacity-80">Service</span> Area</h1>
-                        <p className="m-2 md:m-6 lg:mt-12 text-gray-600 dark:text-gray-300 text-lg md:text-lg w-auto">Our primary service area encompasses the Bozeman community and Gallatin County. Check out the list below for communities we commonly serve.</p>
+                        <p className="m-2 md:m-6 lg:mt-12 text-gray-600 dark:text-gray-300 text-lg md:text-lg w-auto">Our primary service area encompasses the greater Bozeman area community and Gallatin County. Check out the list below for communities we commonly serve.</p>
                     </div>
                 // </div>
             // </div>
