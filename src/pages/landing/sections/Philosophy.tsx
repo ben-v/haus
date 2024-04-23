7b75b8d60f45db926fefb25a04a90102a472e107
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-22 20:52:46 -0600

    Refined landing page spacing.

--- src/pages/landing/sections/Philosophy.tsx
+++ src/pages/landing/sections/Philosophy.tsx
@@ -10,14 +10,14 @@ const Philosophy = () => {
 
     return (
         <ContentSection flexDirection="RTL">
-            <div className="lg:w-8/12 w-auto p-2 md:pt-6">
+            <div className="md:w-7/12 w-auto">
                 <ContenSectiontHeader
                     title="Old School Meets New" 
                     titleAlignment="Left"
                     description={`At <span className="font-semibold">HAUS</span> our approach with our clients is first and foremost is to help each and every one of them with their list of projects, big or small. With over over 30 years in high tech, at some of the largest companies in the United States, respectful and considerate communication & collaboration were the foundations of success then and are the foundation of success today on any project.`}
                     descriptionAlignment="Left" />
             </div>
-            <div className="lg:w-4/12 w-auto p-4 mt-4 md:mt-3">
+            <div className="md:w-5/12 w-auto">
                 {(theme === THEMES.DARK) ? <SvgIntroSuccessDark className="w-full" /> : <SvgIntroSuccessLight className="w-full" />}
             </div>
         </ContentSection>
