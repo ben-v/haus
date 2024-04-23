e6a4a4b05ff8fc9b7dddb88830c1609feabf0a68
Ben <8211835+ben-v@users.noreply.github.com>
2024-04-23 01:18:42 -0600

    For now, just init faqs to all open state because open/close animation on mobile is janky. Fix later

--- src/pages/faqs/sections/FaqsDetailSection.tsx
+++ src/pages/faqs/sections/FaqsDetailSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ContentSection from "../../../components/layouts/ContentSection";
+import RoundCornerContainer from '../../../components/containers/RoundCornerContainer';
 
 // NOTE: In order to get the Accordian component to work from Material-Tailwind, had to downgrade to React 18.2.42. 
 // For more info see https://stackoverflow.com/questions/78296875/typescript-error-using-material-tailwind-react-with-nextjs14
@@ -10,20 +11,20 @@ import {
   AccordionBody,
 } from "@material-tailwind/react";
 
-function Icon({ id, open } : {id: number, open: number}) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-      className={`${id === open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-    </svg>
-  );
-}
+// function Icon({ id, open } : {id: number, open: number}) {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       strokeWidth={2}
+//       stroke="currentColor"
+//       className={`${id === open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
+//     >
+//       <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+//     </svg>
+//   );
+// }
 
 const faqs: { question: string, answer: string }[] = [
   {
@@ -54,20 +55,27 @@ const faqs: { question: string, answer: string }[] = [
 
 const FaqsDetailSection = () => {
   // Init using -1 because indexes start at 0
-  const [open, setOpenState] = React.useState(-1);
+  // const [open, setOpenState] = React.useState(-1);
+  
+  // Accordian animation on mobile phone is bad, so just open all for now until have better solution. 4/23/24
+  const [alwaysOpen, setAlwaysOpen] = React.useState(true);
+
+  const handleAlwaysOpen = () => setAlwaysOpen((cur) => !cur);
 
   // If closing, use -1 because indexes start at 0
-  const onClickHandler = (index: React.SetStateAction<number>) => setOpenState(open === index ? -1 : index);
+  // const onClickHandler = (index: React.SetStateAction<number>) => setOpenState(open === index ? -1 : index);
 
   return (
     <ContentSection flexDirection="Center">
-      {faqs.map((faq, index) => (
-        <Accordion open={open === index} icon={<Icon id={index} open={open} />}>
-          <AccordionHeader onClick={() => onClickHandler(index)} className="accordian-header">{faq.question}</AccordionHeader>
+      <RoundCornerContainer>
+      {faqs.map((faq,) => (
+        <Accordion open={alwaysOpen}>
+          <AccordionHeader onClick={handleAlwaysOpen} className="accordian-header">{faq.question}</AccordionHeader>
           <AccordionBody className="accordian-body">{faq.answer}</AccordionBody>
         </Accordion>
 
       ))}
+      </RoundCornerContainer>
     </ContentSection>
   );
 }
